58cac3da810eecd0a42d2700f842fa92196557a4
paul <paul@cueb.io>
2017-01-14 16:52:46 -0800

    m update

--- m/bootstrap.sh
+++ m/bootstrap.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
+M_VENV=/usr/local/bin/m_venv
+
+if [ ! -d "$M_VENV" ]; then
+    (cd /usr/local/bin && virtualenv m_venv)
+fi
+
+/usr/local/bin/m_venv/bin/pip install -r pip-requirements.txt
+
+rm -f /usr/local/bin/m
 ln -s `pwd`/m /usr/local/bin/m
+
+echo "Setup complete"
