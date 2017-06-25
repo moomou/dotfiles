0b09a641362d79617e193674eef3fdc9b6a1f440
paul <paul@cueb.io>
2017-06-24 23:07:14 -0700

    update

--- bootstrap_nvim.sh
+++ bootstrap_nvim.sh
@@ -2,10 +2,11 @@
 
 pyenv install 3.4.4
 pyenv virtualenv 3.4.4 neovim3
+
 pyenv activate neovim3
-pip install neovim
-pyenv which python  # Note the path
-pip install yapf
+pip install neovim yapf flake8
+
+ln -s `pyenv which flake8` ~/.local/bin/flake8
+ln -s `pyenv which yapf` ~/.local/bin/yapf
 
 echo `which python3`
-pyenv deactivate neovim3
