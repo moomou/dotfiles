f2a9b6a5f6ac457988a371e8e677033a6809496c
paul <paul@cueb.io>
2017-08-31 21:22:12 -0700

    improve m_docker

--- m/m_docker.py
+++ m/m_docker.py
@@ -1,4 +1,5 @@
 import os
+import contextlib
 import shutil
 
 import constant
@@ -16,17 +17,26 @@ class Docker(Base):
 
     def build_2_gz(self, app, name=None):
         name = name or app
-        self.shell('docker build . -t %s' % app)
+
+        self.build(app)
         self.shell('docker save %s | gzip -c > %s.gz' % (app, name))
 
     def build_2_gcloud(self, app, name=None):
         self.build_2_gz(app, name)
-        
+
         self.shell(
             'gsutil -o GSUtil:parallel_composite_upload_threshold=150M cp %s gs://moomou2/%s.gz'
             % name)
 
-    def build(self, app):
+    def build_2_chub(self, app, name=None):
+        self.build(app, True)
+
+        self.shell('docker save %s | gzip -c > ~/dev/chub/%s.gz' % (app, app))
+        cmd = 'cd %s && git add . && git ci -m updated %s.gz && git push origin master' % (
+            m_path('chub'), app)
+        self.shell(cmd)
+
+    def build(self, app, include_cert=False):
         '''Build a docker image of the app and push to remote repo'''
         config_yml = get_config_yml()
         remove_after = ['./gitlab_rsa', './.dockerignore']
@@ -37,17 +47,16 @@ class Docker(Base):
                 remove_after.append(dst)
 
         # Copy gitlab cert
-        shutil.copy2(
-            os.path.join(constant.SSH_DIR, 'key', 'gitlab_rsa'),
-            './gitlab_rsa')
+        if include_cert:
+            shutil.copy2(
+                os.path.join(constant.SSH_DIR, 'key', 'gitlab_rsa'),
+                './gitlab_rsa')
         # Copy over dockerignore
         shutil.copy2(
             os.path.join(constant.M_ROOT, '.dockerignore'), './.dockerignore')
 
         self.shell('docker build . -t %s' % app)
-        self.shell('docker save %s | gzip -c > ~/dev/chub/%s.gz' % (app, app))
-        [os.remove(f) for f in remove_after]
 
-        cmd = 'cd %s && git add . && git ci -m updated %s.gz && git push origin master' % (
-            m_path('chub'), app)
-        self.shell(cmd)
+        with contextlib.suppress(FileNotFoundError):
+            for f in remove_after:
+                os.remove(f)
