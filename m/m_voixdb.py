276f0184769d596040ea8a66c011b3a3b2a00b27
paul <paul@cueb.io>
2018-06-19 22:39:13 -0700

    fixes

--- m/m_voixdb.py
+++ m/m_voixdb.py
@@ -19,7 +19,7 @@ class Voixdb(Base):
             'librosa',
         ])
 
-    def register(self, dir, dataset_id='0', server='localhost:5000'):
+    def register(self, dir, dataset_id='0', server='http://localhost:5000'):
         '''
         Given a data dir prepared by m_audio.gather_youtube_segments, register these
         voixdb instance.
@@ -31,9 +31,14 @@ class Voixdb(Base):
             if f.endswith('mp3'):
                 ytid, spkid, start_time, end_time = f[:-4].split('~')
                 data, sr = librosa.core.load(
-                    f, sr=SAMPLE_RATE, duration=30, mono=True)
+                    os.path.join(dir, f),
+                    sr=SAMPLE_RATE,
+                    duration=30,
+                    mono=True)
 
-                file = io.BytesIO(json.dumps(data.tolist()).encode('utf-8'))
+                # skip '[' and ']'
+                file = io.BytesIO(
+                    json.dumps(data.tolist())[1:-1].encode('utf-8'))
 
                 requests.post(
                     server + '/register/%s/%s' % (dataset_id, spkid),
@@ -41,5 +46,5 @@ class Voixdb(Base):
                         'bin': file,
                     },
                     data={'meta': json.dumps({
-                        sr: SAMPLE_RATE,
+                        'sr': SAMPLE_RATE,
                     })})
