1f8fe30e611461e1446f0535b8ffe9cd2eb525d8
paul <paul@cueb.io>
2018-07-07 23:09:33 -0700

    use recommended fixes

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# assume debian
-sudo apt-get install -y build-essential
+# assume debian from https://github.com/pyenv/pyenv/wiki/Common-build-problems#requirements
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
+libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
+xz-utils tk-dev libffi-dev
 
 # install pyenv
 curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
