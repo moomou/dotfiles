9e314ccb7c9917bcb1ad28c68991b454e36eb877
paul <paul@cueb.io>
2019-02-02 18:42:26 -0800

    tweak

--- chef/bootstrap.sh
+++ chef/bootstrap.sh
@@ -6,7 +6,7 @@ role="${2:-box}"
 
 # The host key might change when we instantiate a new VM, so
 # we remove (-R) the old host key from known_hosts
-ssh-keygen -R "${host#*@}" 2> /dev/null
+ssh-keygen -R "${host#*@}" 2>/dev/null
 
 echo Running role $role...
 echo
--- chef/run_chef_role.sh
+++ chef/run_chef_role.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
+
+RUBY_VERSION=ruby2.4
+CHEF_BINARY=chef-solo
+
 role=$1
 
-# This runs as root on the server
-chef_binary=chef-solo
-
 # Are we on a vanilla system?
-if ! command -v $chef_binary >/dev/null 2>&1; then
+if ! command -v $CHEF_BINARY >/dev/null 2>&1; then
     export DEBIAN_FRONTEND=noninteractive
-    # Upgrade headlessly (this is only safe-ish on vanilla systems)
-    #apt-get -o Dpkg::Options::="--force-confnew" \
-        #--force-yes -fuy dist-upgrade &&
-    # Install Ruby, Chef, and its dep
-    apt-get update &&
-    apt-get install -y \
-        ruby2.3 \
-        ruby2.3-dev \
-        make \
-        libgmp-dev \
-        gcc \
-        &&
 
-    sudo gem2.3 install --no-rdoc --no-ri chef berkshelf
+    # Install Ruby, Chef, and its dep
+    if ! $(apt-cache search $RUBY_VERSION | grep -q $RUBY_VERSION); then
+        apt-add-repository ppa:brightbox/ruby-ng -y
+    fi
+
+    apt-get update &&
+        apt-get install -y \
+            $RUBY_VERSION \
+            $RUBY_VERSION-dev \
+            make \
+            libgmp-dev \
+            gcc &&
+        sudo gem2.4 install --no-rdoc --no-ri chef berkshelf
 fi
 
 # install dep cookbooks
 sudo berks vendor ./vendor
 
 # run chef
-sudo "$chef_binary" -c solo.rb -j "./roles/${role}.json"
+sudo "$CHEF_BINARY" -c solo.rb -j "./roles/${role}.json"
