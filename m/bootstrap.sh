d67f5e86855ece1cc8217f28a702e734cc05d839
paul <paul@cueb.io>
2019-02-12 23:48:27 -0800

    checkpoint

--- m/bootstrap.sh
+++ m/bootstrap.sh
@@ -6,7 +6,7 @@ sudo apt-get install -y libsndfile1 libsndfile1-dev
 FNAME=m_venv3
 M_VENV=/usr/local/bin/$FNAME
 
-PY3=`which python3`
+PY3=$(which python3)
 if [ ! -d "$M_VENV" ]; then
     (cd /usr/local/bin && python3 -m venv $FNAME)
 fi
@@ -16,6 +16,6 @@ $M_VENV/bin/pip install numpy
 $M_VENV/bin/pip install -r pip-requirements.txt
 
 rm -f /usr/local/bin/m
-ln -s `pwd`/m /usr/local/bin/m
+ln -s $(pwd)/m /usr/local/bin/m
 
 echo "Setup complete"
