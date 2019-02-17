a305ab112dc56e552caadc105cead264d979d64f
paul <paul@cueb.io>
2019-02-17 14:42:39 -0800

    nits

--- shell_lib/bootstrap_mac.sh
+++ shell_lib/bootstrap_mac.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # assume debian from https://github.com/pyenv/pyenv/wiki/Common-build-problems#requirements
-brew install ripgrep bat fd ncdu tldr dep
+brew install adns ant autoconf bat bazel cabextract coreutils dart@2 dep doctl fd ffmpeg flac gdbm gettext git git-lfs gmp gnutls go htop hugo jemalloc jq lame libassuan libevent libffi libgcrypt libgpg-error libidn2 libksba libogg libpng libsndfile libssh2 libtasn1 libtermkey libunistring libusb libuv libvorbis libvterm libzip luajit mad mkcert moreutils msgpack n ncdu neovim nettle npth oniguruma openssl p11-kit p7zip pcre pcre2 phantomjs pinentry pipenv pkg-config prototool pup pyenv pyenv-virtualenv python python@2 readline redis ripgrep sass shfmt sox sqlite the_silver_searcher tldr tmux torsocks tree unibilium unrar watch watchman wget wine winetricks x264 xvid xz youtube-dl zlib
