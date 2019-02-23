00db8f7c03f37adb009529b5f36382bf7474a145
paul <paul@cueb.io>
2019-02-22 19:52:47 -0800

    simple cleanup

--- m/bootstrap.sh
+++ m/bootstrap.sh
@@ -6,7 +6,6 @@ sudo apt-get install -y libsndfile1 libsndfile1-dev
 FNAME=m_venv3
 M_VENV=/usr/local/bin/$FNAME
 
-PY3=$(which python3)
 if [ ! -d "$M_VENV" ]; then
     (cd /usr/local/bin && python3 -m venv $FNAME)
 fi
@@ -18,4 +17,4 @@ $M_VENV/bin/pip install -r pip-requirements.txt
 rm -f /usr/local/bin/m
 ln -s $(pwd)/m /usr/local/bin/m
 
-echo "Setup complete"
+echo "m setup complete"
