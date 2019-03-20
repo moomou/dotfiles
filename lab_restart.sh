af48ec9e02c2a3a96a155463c173d007509821ac
paul <paul@cueb.io>
2019-03-19 20:17:54 -0700

    basic lab restart sh

---
+++ lab_restart.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+STATUS=$(curl -s https://lab.ohsloth.com | awk '{print $1}')
+
+echo "[$(date)]" $STATUS
+
+if [ "$STATUS" == "OK" ]; then
+    sleep 30
+else
+    echo restarting...
+    supervisorctl restart spa_tunnel
+    sleep 30
+fi
