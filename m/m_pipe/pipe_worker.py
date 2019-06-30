280f032e6dfb16e51fe50c9bcdc6e498042fbf59
paul <paul@cueb.io>
2019-06-30 14:33:09 -0400

    fix pipe commiter check condition

--- m/m_pipe/pipe_worker.py
+++ m/m_pipe/pipe_worker.py
@@ -29,12 +29,11 @@ class Committer(threading.Thread):
     def _parse_log(self):
         completed = set()
         failed = set()
-
         if os.path.exists(self.log_path):
             with open(self.log_path) as f:
                 for line in f:
                     task_id, status = line.split("~~")
-                    if status != "0":
+                    if status.strip() != "0":
                         failed.add(task_id)
                     else:
                         completed.add(task_id)
