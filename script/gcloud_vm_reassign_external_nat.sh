ff3326dcc9b3aab53897ce3dbd68cb121de6515f
paul <paul@cueb.io>
2019-07-26 23:47:02 -0700

    scrip fixes

--- script/gcloud_vm_reassign_external_nat.sh
+++ script/gcloud_vm_reassign_external_nat.sh
@@ -12,16 +12,16 @@ if ! hash jq 2>/dev/null; then
 fi
 
 function get_external_nat() {
-    RES=$(gcloud compute instances describe $1 --format json | jq .networkInterfaces[0].accessConfigs[0] -c)
+    RES=$(gcloud compute instances describe "$1" --zone "$2" --format json | jq .networkInterfaces[0].accessConfigs[0] -c)
     echo "$RES"
 }
 
-NAT=$(get_external_nat "$VM")
+NAT=$(get_external_nat "$VM" "$ZONE")
 # remove
 gcloud compute instances delete-access-config "$VM" --zone "$ZONE" --access-config-name "$(echo $NAT | jq -r .name)"
 
 # add
 gcloud compute instances add-access-config "$VM" --zone "$ZONE"
-NAT=$(get_external_nat "$VM")
+NAT=$(get_external_nat "$VM" "$ZONE")
 
 echo $(echo "$NAT" | jq -r .natIP)
