63d5d9ae4d805d25bfeab289a09e56c4e50484ad
paul <paul@cueb.io>
2019-07-26 21:31:51 -0700

    more pipe work

--- m/m_pipe/pipe_master.py
+++ m/m_pipe/pipe_master.py
@@ -10,6 +10,9 @@ class PipeMaster(Base):
         self.redis_addr = redis_addr
 
     def start(self, event_script):
+        """
+        event_script: relies on python-fire to parse the incoming json to dictionary
+        """
         self._logger.info(
             "Starting pipe master with config:: {config}".format(config=event_script)
         )
@@ -30,13 +33,22 @@ class PipeMaster(Base):
             event = msg["data"]
             if type(event) is bytes and event.decode("utf-8") in event_script:
                 script = event_script[event.decode("utf-8")]
-                retcode, _, _ = self.shell(script)
+                retcode, stdout, err = self.shell(script)
 
-                self._logger.info(
-                    "Got {event} and ran {script} with ret {retcode}".format(
-                        event=event, script=script, retcode=retcode
+                if err:
+                    self._logger.error(
+                        "event:{event} triggered {script} with ret:{retcode} and err:{err}".format(
+                            event=event, script=script, retcode=retcode, err=err
+                        )
                     )
-                )
+                else:
+                    self._logger.info(
+                        "event:{event} triggered {script} with ret:{retcode} and output:{stdout}".format(
+                            event=event, script=script, retcode=retcode, stdout=stdout
+                        )
+                    )
+
+                r.publish("pipe_worker", stdout.decode("utf-8"))
             else:
                 self._logger.info("discarded msg:: %s", event)
 
