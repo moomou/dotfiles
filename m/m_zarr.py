c8e9d68d8ffd047daf67182072c90355e2ae1ede
paul <paul@cueb.io>
2020-03-06 21:42:16 -0800

    fixes zarr ls cmd

--- m/m_zarr.py
+++ m/m_zarr.py
@@ -10,13 +10,11 @@ class MZarr(Base):
     def ls(self, path, key=None):
         zarr = self._module("zarr")
 
-        if not path or path.endswith("zarr"):
+        if not path or not path.endswith("zarr"):
             self._logger.info("Requires a single path ending in zarr")
 
         arr = zarr.open(path, mode="r")
         if key is not None:
             for k in key.split("."):
                 arr = arr[k]
-            print(arr.keys())
-        else:
-            print(arr.keys())
+        print(list(arr.keys()))
