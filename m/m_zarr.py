84519a5ccf233d92f40bab7e53a53ac5e4455eb6
paul <paul@cueb.io>
2020-03-06 21:44:49 -0800

    pprint + fix key

--- m/m_zarr.py
+++ m/m_zarr.py
@@ -5,16 +5,18 @@ from m_base import Base
 
 class MZarr(Base):
     def __init__(self):
-        super(MZarr, self).__init__(["zarr"])
+        super(MZarr, self).__init__(["zarr", "pprint"])
 
     def ls(self, path, key=None):
         zarr = self._module("zarr")
+        pprint = self._module("pprint")
 
         if not path or not path.endswith("zarr"):
             self._logger.info("Requires a single path ending in zarr")
 
         arr = zarr.open(path, mode="r")
         if key is not None:
-            for k in key.split("."):
+            for k in key.split("/"):
                 arr = arr[k]
-        print(list(arr.keys()))
+
+        pprint.pprint(list(arr.keys()))
