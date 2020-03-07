53693bab0b0f808b4de9ef57317653313798e442
paul <paul@cueb.io>
2020-03-06 21:36:33 -0800

    add missing file m_zarr.py

---
+++ m/m_zarr.py
@@ -0,0 +1,22 @@
+import os
+
+from m_base import Base
+
+
+class MZarr(Base):
+    def __init__(self):
+        super(MZarr, self).__init__(["zarr"])
+
+    def ls(self, path, key=None):
+        zarr = self._module("jinja2")
+
+        if not path or path.endswith("zarr"):
+            self._logger.info("Requires a single path ending in zarr")
+
+        arr = zarr.open(path, mode="r")
+        if key is not None:
+            for k in key.split("."):
+                arr = arr[k]
+            print(arr.keys())
+        else:
+            print(arr.keys())
