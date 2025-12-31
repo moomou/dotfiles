75cc46263f3f60df9292e63d2fa19bebb29f8c22
pmou <p@mou.dev>
2025-12-31 20:52:37 +0000

    tmux cfg fixes

--- .tmux_color.sh
+++ .tmux_color.sh
@@ -2,4 +2,7 @@
 # ~/.tmux_color.sh
 VAL=$(hostname | md5sum | cut -c1-2)
 DEC=$((16#$VAL))
-echo "color$(( (DEC % 200) + 16 ))"
+COLOR="color$(( (DEC % 200) + 16 ))"
+
+# Tell tmux to set the style
+tmux set -g status-style "bg=$COLOR,fg=black"
